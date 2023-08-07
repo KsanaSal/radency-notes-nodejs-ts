f3ea8c6d62d79660328a700518a0a3bb9b032807
KsanaSal <oasalivon@gmail.com>
2023-08-07 12:26:15 +0300

    + refactored code

---
+++ src/controller/notes.ts
@@ -0,0 +1,95 @@
+import notesOperations from '../data/notes.js';
+import HttpError from '../helpers/httpError.js';
+
+const getAll = async (req, res, next) => {
+  try {
+    const notes = await notesOperations.listNotes();
+    res.json({ status: 'success', code: 200, data: { result: notes } });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getByStats = async (req, res, next) => {
+  try {
+    const notes = await notesOperations.statsNotes();
+    res.json({ status: 'success', code: 200, data: { result: notes } });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const note = await notesOperations.getNotesById(id);
+    if (!note) {
+      throw HttpError(404);
+    }
+    res.json({
+      status: 'success',
+      code: 200,
+      data: { note },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await notesOperations.deleteNote(id);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.json({
+      status: 'success',
+      code: 200,
+      data: { message: 'contact deleted' },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const postNote = async (req, res, next) => {
+  try {
+    const note = await notesOperations.addNote(req.body);
+    res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: { note },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const patchById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await notesOperations.updateNote(id, req.body);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.json({
+      status: 'success',
+      code: 200,
+      data: { result },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const controller = {
+  getAll,
+  getByStats,
+  getById,
+  deleteById,
+  postNote,
+  patchById,
+};
+
+export default controller;
--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -2,91 +2,20 @@ import express from 'express';
 
 const router = express.Router();
 
-import notesOperations from '../data/notes.js';
-import HttpError from '../helpers/httpError.js';
 import validation from '../middlewares/validation.js';
 import schemas from '../schemas/note.js';
+import controller from '../controller/notes.js';
 
-router.get('/', async (req, res, next) => {
-  try {
-    const notes = await notesOperations.listNotes();
-    res.json({ status: 'success', code: 200, data: { result: notes } });
-  } catch (error) {
-    next(error);
-  }
-});
+router.get('/', controller.getAll);
 
-router.get('/stats', async (req, res, next) => {
-  try {
-    const notes = await notesOperations.statsNotes();
-    res.json({ status: 'success', code: 200, data: { result: notes } });
-  } catch (error) {
-    next(error);
-  }
-});
+router.get('/stats', controller.getByStats);
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const note = await notesOperations.getNotesById(id);
-    if (!note) {
-      throw HttpError(404);
-    }
-    res.json({
-      status: 'success',
-      code: 200,
-      data: { note },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.get('/:id', controller.getById);
 
-router.delete('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await notesOperations.deleteNote(id);
-    if (!result) {
-      throw HttpError(404);
-    }
-    res.json({
-      status: 'success',
-      code: 200,
-      data: { message: 'contact deleted' },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete('/:id', controller.deleteById);
 
-router.post('/', validation(schemas.noteSchema), async (req, res, next) => {
-  try {
-    const note = await notesOperations.addNote(req.body);
-    res.status(201).json({
-      status: 'success',
-      code: 201,
-      data: { note },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.post('/', validation(schemas.noteSchema), controller.postNote);
 
-router.patch('/:id', validation(schemas.patchNoteSchema), async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await notesOperations.updateNote(id, req.body);
-    if (!result) {
-      throw HttpError(404);
-    }
-    res.json({
-      status: 'success',
-      code: 200,
-      data: { result },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.patch('/:id', validation(schemas.patchNoteSchema), controller.patchById);
 
 export default router;
